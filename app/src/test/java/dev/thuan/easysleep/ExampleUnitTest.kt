65d2824096d10460e096ecf202025f73ff8d149b
thuanpham582002 <95533596+thuanpham582002@users.noreply.github.com>
2023-07-23 19:10:54 +0700

    init

---
+++ app/src/test/java/dev/thuan/easysleep/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package dev.thuan.easysleep
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
